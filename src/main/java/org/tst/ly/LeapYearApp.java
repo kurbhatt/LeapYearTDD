58f1c3e74ebd6b83e69608dccc87cb663518f398
Keyur Bhatt <keyur@supertax.in>
2020-04-30 10:55:45 +0530

    004-adding calling code in original source file along with messages

--- src/main/java/org/tst/ly/LeapYearApp.java
+++ src/main/java/org/tst/ly/LeapYearApp.java
@@ -3,7 +3,13 @@ package org.tst.ly;
 public class LeapYearApp {
 
     public static void main(String[] args) {
-
+        int year = 2020;
+        boolean isLeapYear = isLeapYear(year);
+        if (isLeapYear) {
+            System.out.println(String.format("You need to plan for an extra day as %d is leap year", isLeapYear));
+        } else {
+            System.out.println(String.format("No worries %d is not leap year", year));
+        }
     }
 
     public static boolean isLeapYear(int year) {
